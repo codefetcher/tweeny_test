664d77523bec570c9359bd6168638c2bb23cdc87
atharva gomekar <magmayank95@gmail.com>
2016-09-03 21:59:13 +0530

    commit after mongodb

--- fast_rest.py
+++ fast_rest.py
@@ -1,69 +1,55 @@
 from flask import Flask, jsonify,request
-import json
 from flask_restful import Resource, Api
-
-
-
+from pymongo import MongoClient
+from bson.json_util import dumps
 app = Flask(__name__)
 api = Api(app)
+c = MongoClient()
+db=c.food
 
-orders=[
-    {
-      "cust_address": u"katraj", 
-      "cust_contact": 9923593344, 
-      "cust_name": u"mayank", 
-      "cust_pincode": 411043, 
-      "order_description": u"less spicy", 
-      "order_id": 1
-    }, 
-    {
-      "cust_address": u"katraj", 
-      "cust_contact": 9923593344, 
-      "cust_name": u"gupta", 
-      "cust_pincode": 411043, 
-      "order_description":u"spicy", 
-      "order_id": 2
-    }
-  ]
-
+m=db.count.insert_one({"count":1})
 
 @app.route('/getorder', methods=['GET'])
 def get_orders():
-	return jsonify( {"orders":orders})
+	cursor=db.orders.find()
+	#cursor=str(cursor)
+	return dumps(cursor)
 
-@app.route('/getorder/<int:orderid>',methods	=['GET'])
+@app.route('/getorder/<int:orderid>',methods=['GET'])
 def get_order(orderid):
-	
-	for order in orders:
-		if order['order_id']==orderid:
-			curr_order=[order]
-	return jsonify({"order":curr_order[0]})
+	cursor=db.orders.find_one({"order_id":orderid})
+	return dumps(cursor)
 
 
 
 @app.route('/saveorder',methods=['POST'])
 def save_order():
-	order={
-	'order_id':orders[-1]['order_id']+1	,
+	cursor=db.count.find_one()
+	count=cursor['count']
+	count=int(count)
+	count+=1
+	db.orders.insert_one({
+	'order_id':count,
 	'cust_address':request.json['cust_address'],
 	'cust_contact':request.json['cust_contact'],
 	'cust_name':request.json['cust_name'],
 	'cust_pincode':request.json['cust_pincode'],
 	'order_description':request.json['order_description']
-	}
-	orders.append(order)
-	return jsonify({"order":order})
+	})
+	temp=count-1;
+	re=db.count.update_one({"count":temp},{"$set":{"count":count}})
+	return dumps(db.orders.find_one({"order_id":count}))
 
 @app.route('/saveorder/<int:orderid>',methods = ['POST'])
 def update_order(orderid):
-	for order in orders:
-		if order['order_id']==orderid:
-			order[0]['cust_address']=request.json['cust_address']
-			order[0]['cust_contact']=request.json['cust_contact']
-			order[0]['cust_name']=request.json['cust_name']
-			order[0]['cust_pincode']=request.json['cust_pincode']
-			order[0]['order_description']=request.json['order_description']
-			return jsonify({"orders":order})
+	db.orders.update_one({"order_id":orderid},{"$set":{
+			'cust_address':request.json['cust_address'],
+			'cust_contact':request.json['cust_contact'],
+			'cust_name':request.json['cust_name'],
+			'cust_pincode':request.json['cust_pincode'],
+			'order_description':request.json['order_description']
+			}})
+	return dumps(db.orders.find_one({"order_id":orderid}))
 
 
 
